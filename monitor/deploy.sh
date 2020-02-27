014ef4cb3ba29be94f1a41942468dc1cb8e87223
anonymous <>
2020-02-27 16:13:58 +0800

    update deploy

--- monitor/deploy.sh
+++ monitor/deploy.sh
@@ -13,11 +13,13 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
+git config user.name 'anonymous'
 git add -A
 git commit -m 'deploy'
-
+git checkout -b gh-pages
+git remote add origin git@github.com:caysnuts/monitor.git
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:caysnuts/monitor.git master:gh-pages
+git push origin -f gh-pages:gh-pages
